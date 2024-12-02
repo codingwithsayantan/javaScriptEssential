372f1d8870994b0c5be58dfaf98cd3446dabd0c0
= <=>
2024-12-02 14:15:47 -0500

    Fix the js code

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -3,7 +3,7 @@ let width;
 
 function calculateArea() {
     length = parseFloat(document.getElementById('length').value);
-    width = parseFloat(document.getElementById('width').value));
+    width = parseFloat(document.getElementById('width').value);
     let area = length * width;
     document.getElementById('result').innerText = `The area is ${area}` ;
 }
